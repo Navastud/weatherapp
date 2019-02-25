d5c512d25cbf696e475ff2f1adc12b64c97193d8
navastud <david.navastud@gmail.com>
2019-02-25 17:50:55 -0300

    Ejecucion de Fetch Forecast

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,19 +3,42 @@ import PropTypes from "prop-types";
 import ForecastItem from "./ForecasItem";
 import "./styles.css";
 
-const days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+const api_key = "d849795d81f8be2ac7109436ff202f0b";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
 
 class ForecastExtended extends Component {
+  constructor() {
+    super();
+    this.state = { forecastData: null };
+  }
+  componentDidMount() {
+    //fetch or axios
+    const url_forecast = `${url}?q=${this.props.city}&APPID=${api_key}`;
+    fetch(url_forecast)
+      .then(data => data.json())
+      .then(weather_data => {
+        console.log(weather_data);
+      });
+  }
+
   renderForecastItemDays() {
-    return days.map(day => <ForecastItem weekday={day} />);
+    return "Render Item";
+    /*  return days.map(day => (
+      <ForecastItem weekday={day} hour={10} data={data} />
+    )); */
+  }
+
+  renderProgress() {
+    return <h3>Cargando pronostico extendido</h3>;
   }
 
   render() {
     const { city } = this.props;
+    const { forecastData } = this.state;
     return (
       <div>
         <h2 className="forecast-title">Pronóstico Extendido para {city}</h2>
-        {this.renderForecastItemDays()}
+        {forecastData ? this.renderForecastItemDays() : this.renderProgress()}
       </div>
     );
   }
