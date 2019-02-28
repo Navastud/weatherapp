f9c37b2175ac03f12a0eb07b53867b70cf096b1d
navastud <david.navastud@gmail.com>
2019-02-28 17:01:18 -0300

    Refactor LocationList a LocationListContainer

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import LocationList from "./../components/LocationList";
-import { setCity } from "./actions";
+import { setCity } from "./../actions/";
 
 class LocationListContainer extends Component {
   handlerSelectedLocation = city => {
