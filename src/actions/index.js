f515f3c721a4beb7f1746d13e24eb4f6ced40b26
navastud <david.navastud@gmail.com>
2019-03-05 12:29:50 -0300

    Implementacion de Redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,33 @@
+import transformForecast from "./../services/transformForecast";
+
 export const SET_CITY = "SET_CITY";
+export const SET_FORECAST_DATA = "SET_FORECAST_DATA";
 
 export const setCity = payload => ({ type: SET_CITY, payload });
+export const setForecastData = payload => ({
+  type: SET_FORECAST_DATA,
+  payload
+});
+
+const api_key = "d849795d81f8be2ac7109436ff202f0b";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
+export const setSelectedCity = payload => {
+  return dispatch => {
+    const url_forecast = `${url}?q=${payload}&APPID=${api_key}`;
+
+    // activar en el estado un indicador de busqueda de datos
+
+    dispatch(setCity(payload));
+
+    fetch(url_forecast)
+      .then(data => data.json())
+      .then(weather_data => {
+        const forecastData = transformForecast(weather_data);
+
+        // modificar el estado con el resultado de la promise (fetch)
+        dispatch(-{ city: payload, forecastData });
+      });
+    return;
+  };
+};
