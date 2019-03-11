ed02d12da33c57768d2de2043a95656ac5073299
navastud <david.navastud@gmail.com>
2019-03-11 11:47:40 -0300

    implementacion de reselect

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { createSelector } from "reselect";
 import {
   cities,
   getForecastDataFromCities as _getForecastDataFromCities
@@ -7,7 +8,13 @@ import { city } from "./city";
 
 export default combineReducers({ cities, city });
 
-export const getCity = state => state.city;
+export const getCity = createSelector(
+  state => state.city,
+  city => city
+);
 
-export const getForecastDataFromCities = state =>
-  _getForecastDataFromCities(state.cities, getCity(state));
+export const getForecastDataFromCities = createSelector(
+  state => state.cities,
+  getCity,
+  _getForecastDataFromCities
+);
