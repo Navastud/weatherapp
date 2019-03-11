8485d24e7ff581929b10d6cc7b337a90dba54eea
navastud <david.navastud@gmail.com>
2019-03-11 16:36:48 -0300

    Utilizacion de getState en Redux Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,12 +21,20 @@ const url = "http://api.openweathermap.org/data/2.5/forecast";
 const url_weather = "http://api.openweathermap.org/data/2.5/weather";
 
 export const setSelectedCity = payload => {
-  return dispatch => {
+  return (dispatch, getState) => {
     const url_forecast = `${url}?q=${payload}&APPID=${api_key}`;
 
     // activar en el estado un indicador de busqueda de datos
 
     dispatch(setCity(payload));
+
+    const state = getState();
+    const date =
+      state.cities[payload] && state.cities[payload].forecastDataDate;
+    const now = new Date();
+    if (date && now - date < 1 * 60 * 1000) {
+      return;
+    }
 
     fetch(url_forecast)
       .then(data => data.json())
